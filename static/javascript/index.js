ef44ee2eb7dbe41de86b11efd3f3cbe1e766daf1
Ethan Gelstein <zinzacrack@gmail.com>
2023-05-18 21:22:48 -0300

    add listener to theme button

---
+++ static/javascript/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const darkModeButton = document.getElementById("dark-mode-button")
+
+  darkModeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode")
+  })
+})
