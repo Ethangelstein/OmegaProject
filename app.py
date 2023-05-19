3aee76638cd040c79b40d55901cc3fcc4e75a24c
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-05-18 22:06:49 -0300

    extract auth logic and add blueprint

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
-import hashlib
-import re
 
-from flask import Flask, request, url_for, redirect, abort, render_template
+from flask import Flask, render_template
 
-from database.database import database
+from auth.auth import auth
 
 app = Flask(__name__)
 
@@ -16,74 +14,12 @@ app.config.from_mapping(
     DATABASE=os.environ.get("FLASK_DATABASE"),
 )
 
-
-db = database.connect()
-
-cursor = db.cursor(
-    dictionary=True,
-)
+app.register_blueprint(auth)
 
 
 @app.route("/", methods=["GET"])
 def main():
     return render_template("index.html")
-
-
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    if request.method == "POST":
-        username = request.form["username"]
-        email = request.form["email"]
-        password = request.form["password"]
-
-        # Checks
-        if is_password_incorrect(password):
-            password_length = len(password)
-            MIN_PASSWORD_LENGTH = 8
-            reason = "menor" if password_length < MIN_PASSWORD_LENGTH else "mayor"
-
-            return throwErrorTemplate(
-                field="Password", reason=reason, value=MIN_PASSWORD_LENGTH
-            )
-
-        if not is_valid_email(email):
-            return throwErrorTemplate(field="Email", reason="igual", value=email)
-
-        sql = "insert into user (username, email, password) values(%s, %s, %s)"
-
-        # Hash password
-        salt = "NJOXSA?!#"
-
-        # Concatenar la contraseña y la salt
-        salted_password = password.encode("utf-8") + salt.encode("utf-8")
-
-        # Calcular el hash SHA256 de la contraseña y la salt
-        hashed_password = hashlib.sha256(salted_password).hexdigest()
-
-        values = (username, email, hashed_password)
-        cursor.execute(sql, values)
-        midb.commit()
-
-        return redirect(url_for("index"))
-    return render_template("signup.html")
-
-
-def is_password_incorrect(password):
-    return len(password) <= 8 or len(password) >= 50
-
-
-def is_valid_email(email):
-    regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-    return bool(re.match(regex, email))
-
-
-def throwErrorTemplate(field, reason, value):
-    return render_template("error.html", field=field, reason=reason, value=value)
-
-
-@app.route("/")
-def index():
-    return "Live :D"
 
 
 if __name__ == "__main__":
