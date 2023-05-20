6f444dfc867474735f000ba6ae66f6267a426b1d
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-05-20 20:02:11 -0300

    add retries for database connection

--- database/database.py
+++ database/database.py
@@ -1,5 +1,6 @@
-import mysql.connector
+import time
 import click
+import mysql.connector
 
 from flask import current_app, g as context
 from flask.cli import with_appcontext
@@ -26,19 +27,28 @@ def init_db():
 
 
 def get_db():
-    try:
-        if "database" not in context:
-            context.database = mysql.connector.connect(
-                host=current_app.config["DATABASE_HOST"],
-                user=current_app.config["DATABASE_USER"],
-                password=current_app.config["DATABASE_PASSWORD"],
-                database=current_app.config["DATABASE"],
-            )
+    max_attempts = 3
+    sleep_duration = 1  # Sleep for 1 second before each retry
 
-            context.cursor = context.database.cursor(dictionary=True)
+    for attempt in range(1, max_attempts + 1):
+        try:
+            if "database" not in context:
+                context.database = mysql.connector.connect(
+                    host=current_app.config.get("DATABASE_HOST"),
+                    user=current_app.config.get("DATABASE_USER"),
+                    password=current_app.config.get("DATABASE_PASSWORD"),
+                    database=current_app.config.get("DATABASE"),
+                )
 
-        return context.database, context.cursor
+                context.cursor = context.database.cursor(dictionary=True)
 
-    except BaseException as error:
-        print("Couldn't connect to MySQL DB")
-        exit(1)
+            return context.database, context.cursor
+
+        except mysql.connector.Error as error:
+            print("Attempt", attempt, "- Couldn't connect to MySQL DB:", error)
+            if attempt < max_attempts:
+                print("Retrying in", sleep_duration, "seconds...")
+                time.sleep(sleep_duration)
+
+    print("Failed to connect to MySQL DB after", max_attempts, "attempts")
+    exit(1)
