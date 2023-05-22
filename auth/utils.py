982029785a7e387144fa95f88c3e6fa9870e5dda
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-05-22 20:04:16 -0300

    create hash password function

--- auth/utils.py
+++ auth/utils.py
@@ -1,4 +1,5 @@
 import re
+import hashlib
 
 
 def is_password_incorrect(password):
@@ -8,3 +9,10 @@ def is_password_incorrect(password):
 def is_valid_email(email):
     regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     return bool(re.match(regex, email))
+
+def hash_password(password):
+    salt = "NJOXSA?!#"
+
+    salted_password = password.encode("utf-8") + salt.encode("utf-8")
+
+    return hashlib.sha256(salted_password).hexdigest()
