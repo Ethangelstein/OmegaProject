575637a3089d073c2ea0171fa764822ce68fbb93
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-05-25 11:53:08 -0300

    add hashed password validation

--- auth/utils.py
+++ auth/utils.py
@@ -10,9 +10,15 @@ def is_valid_email(email):
     regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     return bool(re.match(regex, email))
 
+
 def hash_password(password):
     salt = "NJOXSA?!#"
 
     salted_password = password.encode("utf-8") + salt.encode("utf-8")
 
     return hashlib.sha256(salted_password).hexdigest()
+
+
+def is_valid_password(password, stored_hash):
+    hashed_password = hash_password(password)
+    return hashed_password == stored_hash
